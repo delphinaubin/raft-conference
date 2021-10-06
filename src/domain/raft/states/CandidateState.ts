d344183de45bbb89985fccb07ba86c2afa760630
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2021-10-06 23:50:25 +0200

    feat: add raft election process

--- src/domain/raft/states/CandidateState.ts
+++ src/domain/raft/states/CandidateState.ts
@@ -1,5 +1,6 @@
 import { NodeAlgorithmState } from "@/domain/raft/states/NodeAlgorithmState";
 import { VoteRequestBuilder } from "@/domain/network/VoteRequestBuilder";
+import { VoteResponse } from "@/domain/network/NetworkRequest";
 
 export class CandidateState extends NodeAlgorithmState {
   name = "candidate" as const;
@@ -7,6 +8,10 @@ export class CandidateState extends NodeAlgorithmState {
   async onEnterInState(): Promise<void> {
     await super.onEnterInState();
     // TODO DAU : voir comment afficher le memory dans l'IHM
+    await this.onElectionTimeout();
+  }
+
+  private async onElectionTimeout(): Promise<void> {
     this.nodeMemoryState.term++;
     this.nodeMemoryState.votedFor = this.nodeId;
     this.nodeMemoryState.votesReceived = [this.nodeId];
@@ -22,5 +27,24 @@ export class CandidateState extends NodeAlgorithmState {
         )
       );
     await Promise.all(promises);
+
+    this.startTimer(2_000).then(() => {
+      this.onElectionTimeout();
+    });
+  }
+
+  protected async onVoteResponse(response: VoteResponse): Promise<void> {
+    await super.onVoteResponse(response);
+    /* TODO check that super didn't change this node state to follower   
+       because the term in the response is greater than this node term   
+       If that's the case, this node shoudln't do anything else          
+       (I'm not sure how to implement such a check or if it's even       
+       possible)                                                       */
+    if (this.nodeMemoryState.term == response.term && response.granted) {
+      this.nodeMemoryState.votesReceived.push(response.voterId);
+      if (this.quorumReached(this.nodeMemoryState.votesReceived.length)) {
+        await this.changeState("leader");
+      }
+    }
   }
 }
--- src/domain/raft/states/NodeAlgorithmState.ts
+++ src/domain/raft/states/NodeAlgorithmState.ts
@@ -3,8 +3,13 @@ import { EventBus } from "@/domain/event/EventBus";
 import { ChangeStateEventBuilder } from "@/domain/event/ChangeStateEventBuilder";
 import { TimerManager } from "@/domain/timer/TimerManager";
 import { NodeMemoryState } from "@/domain/raft/AbstractNodeAlgorithm";
-import { NetworkRequest } from "@/domain/network/NetworkRequest";
+import {
+  NetworkRequest,
+  VoteRequest,
+  VoteResponse,
+} from "@/domain/network/NetworkRequest";
 import { NetworkRequestEventBuilder } from "@/domain/event/NetworkEventBuilder";
+import { VoteResponseBuilder } from "@/domain/network/VoteResponseBuilder";
 
 export abstract class NodeAlgorithmState {
   constructor(
@@ -43,8 +48,12 @@ export abstract class NodeAlgorithmState {
     }
   }
 
-  onReceiveNetworkRequest(_request: NetworkRequest): Promise<void> {
-    return Promise.resolve(undefined);
+  async onReceiveNetworkRequest(_request: NetworkRequest): Promise<void> {
+    if (_request.type == "vote-request") {
+      await this.onVoteRequest(_request);
+    } else if (_request.type == "vote-response") {
+      await this.onVoteResponse(_request);
+    }
   }
 
   protected async changeState(newState: RaftNodeState): Promise<void> {
@@ -56,6 +65,9 @@ export abstract class NodeAlgorithmState {
     );
   }
 
+  // TODO return the timerID
+  // The timerID can be used by state algorithm to cancel running timer
+  // (for instance if a node is elected leader, it cancels it's election timer
   protected async startTimer(duration: number): Promise<void> {
     const timerId = this.timerManager.startTimer(duration, this.nodeId);
     return new Promise<void>((resolve) => {
@@ -69,5 +81,59 @@ export abstract class NodeAlgorithmState {
         .withNetworkRequest(request)
         .build()
     );
+  }
+
+  async onVoteRequest(request: VoteRequest): Promise<void> {
+    // message sent by this very node can safely be ignored
+    if (request.senderNodeId != this.nodeId) {
+      // TODO add logOk check
+      // In raft algorithm, an additional check is made to make sure the log of the receiver is coherent
+      // before voting for the sender
+      const termOk =
+        request.term > this.nodeMemoryState.term ||
+        (request.term == this.nodeMemoryState.term &&
+          (this.nodeMemoryState.votedFor == request.senderNodeId ||
+            this.nodeMemoryState.votedFor == null));
+
+      if (termOk) {
+        this.nodeMemoryState.term = request.term;
+        this.nodeMemoryState.votedFor = request.senderNodeId;
+        const promise = this.sendNetworkRequest(
+          VoteResponseBuilder.aVoteResponse()
+            .withSenderNodeId(this.nodeId)
+            .withReceiverNodeId(request.senderNodeId)
+            .withTerm(this.nodeMemoryState.term)
+            .withGranted(true)
+            .build()
+        );
+
+        await promise;
+        if (this.name != "candidate") {
+          await this.changeState("candidate");
+        }
+      } else {
+        const promise = this.sendNetworkRequest(
+          VoteResponseBuilder.aVoteResponse()
+            .withSenderNodeId(this.nodeId)
+            .withReceiverNodeId(request.senderNodeId)
+            .withTerm(this.nodeMemoryState.term)
+            .withGranted(false)
+            .build()
+        );
+        await promise;
+      }
+    }
+  }
+
+  protected async onVoteResponse(response: VoteResponse): Promise<void> {
+    if (response.term > this.nodeMemoryState.term) {
+      this.nodeMemoryState.term = response.term;
+      this.nodeMemoryState.votedFor = undefined;
+      await this.changeState("candidate");
+    }
+  }
+
+  protected quorumReached(numberOfAcks: number): boolean {
+    return numberOfAcks >= Math.ceil(this.allNodesIds.length + 1) / 2;
   }
 }
