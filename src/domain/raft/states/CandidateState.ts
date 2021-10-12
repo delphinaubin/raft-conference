a44547423d913185e5c85410791906b3fa7ded92
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2021-10-12 14:08:15 +0200

    step-0 - no code

--- src/domain/raft/states/CandidateState.ts
+++ src/domain/raft/states/CandidateState.ts
@@ -1,69 +1,9 @@
 import { NodeAlgorithmState } from "@/domain/raft/states/NodeAlgorithmState";
-import { VoteRequestBuilder } from "@/domain/network/VoteRequestBuilder";
-import {
-  BroadcastRequest,
-  LogRequest,
-  VoteResponse,
-} from "@/domain/network/NetworkRequest";
 
 export class CandidateState extends NodeAlgorithmState {
   name = "candidate" as const;
 
   onEnterInState(): void {
     super.onEnterInState();
-    // TODO DAU : voir comment afficher le memory dans l'IHM
-    this.onElectionTimeout();
-  }
-
-  private onElectionTimeout(): void {
-    this.nodeMemoryState.term++;
-    this.nodeMemoryState.votedFor = this.nodeId;
-    this.nodeMemoryState.votesReceived = [this.nodeId];
-    this.startTimer(2_000, "Election timeout").then(() => {
-      this.onElectionTimeout();
-    });
-
-    const lastLogTerm = this.lastLogTerm();
-    this.allNodesIds
-      .filter((nodeId) => nodeId !== this.nodeId)
-      .forEach((otherNodeId) =>
-        this.sendNetworkRequest(
-          VoteRequestBuilder.aVoteRequest()
-            .withSenderNodeId(this.nodeId)
-            .withReceiverNodeId(otherNodeId)
-            .withTerm(this.nodeMemoryState.term)
-            .withLogLength(this.nodeMemoryState.log.length)
-            .withLogTerm(lastLogTerm)
-            .build()
-        )
-      );
-  }
-
-  protected onVoteResponse(response: VoteResponse): void {
-    super.onVoteResponse(response);
-    /* TODO check that super didn't change this node state to follower
-       because the term in the response is greater than this node term
-       If that's the case, this node shoudln't do anything else
-       (I'm not sure how to implement such a check or if it's even
-       possible)                                                       */
-    if (this.nodeMemoryState.term == response.term && response.granted) {
-      this.nodeMemoryState.votesReceived.push(response.voterId);
-      if (this.quorumReached(this.nodeMemoryState.votesReceived.length)) {
-        this.changeState("leader");
-      }
-    }
-  }
-
-  onBroadcastRequest(request: BroadcastRequest): void {
-    // leader is unknown at this time, so do nothing
-  }
-
-  protected onLogRequest(request: LogRequest): void {
-    super.onLogRequest(request);
-    if (request.term >= this.nodeMemoryState.term) {
-      this.nodeMemoryState.leader = request.leaderId;
-      this.cancelTimers();
-      this.startElectionTimer(false);
-    }
   }
 }
--- src/domain/raft/states/NodeAlgorithmState.ts
+++ src/domain/raft/states/NodeAlgorithmState.ts
@@ -30,8 +30,6 @@ export abstract class NodeAlgorithmState {
 
   abstract name: RaftNodeState;
 
-  abstract onBroadcastRequest(request: BroadcastRequest): void;
-
   onEnterInState(): void {
     this.eventBusSubscriberId = this.eventBus.subscribe(({ event }) => {
       if (
@@ -88,7 +86,6 @@ export abstract class NodeAlgorithmState {
     );
   }
 
-  // TODO return the timerID
   // The timerID can be used by state algorithm to cancel running timer
   // (for instance if a node is elected leader, it cancels it's election timer
   protected startTimer(duration: number, label: string): Promise<void> {
@@ -102,67 +99,24 @@ export abstract class NodeAlgorithmState {
     this.networkManager.sendRequest(request);
   }
 
-  onVoteRequest(request: VoteRequest): void {
-    // message sent by this very node can safely be ignored
-    if (request.senderNodeId != this.nodeId) {
-      // In raft algorithm, an additional check is made to make sure the log of the receiver is coherent
-      // before voting for the sender
-      const myLogTerm = this.lastLogTerm();
-      const logOk =
-        request.logTerm > myLogTerm ||
-        (request.logTerm == myLogTerm &&
-          request.logLength >= this.nodeMemoryState.log.length);
-
-      const termOk =
-        request.term > this.nodeMemoryState.term ||
-        (request.term == this.nodeMemoryState.term &&
-          (this.nodeMemoryState.votedFor == request.senderNodeId ||
-            this.nodeMemoryState.votedFor == null));
-
-      // TODO DAU : refactor
-      if (termOk && logOk) {
-        this.nodeMemoryState.term = request.term;
-        this.nodeMemoryState.votedFor = request.senderNodeId;
-        this.changeState("follower");
-        // TODO DAU : check the voterId
-        this.sendNetworkRequest(
-          VoteResponseBuilder.aVoteResponse()
-            .withSenderNodeId(this.nodeId)
-            .withVoterId(this.nodeId)
-            .withGranted(true)
-            .withReceiverNodeId(request.senderNodeId)
-            .withTerm(this.nodeMemoryState.term)
-            .build()
-        );
-      } else {
-        // TODO DAU : check the voterId
-        this.sendNetworkRequest(
-          VoteResponseBuilder.aVoteResponse()
-            .withSenderNodeId(this.nodeId)
-            .withVoterId(this.nodeId)
-            .withReceiverNodeId(request.senderNodeId)
-            .withTerm(this.nodeMemoryState.term)
-            .withGranted(false)
-            .build()
-        );
-      }
-    }
+  onBroadcastRequest(request: BroadcastRequest): void {
+    // DO NOTHING
   }
 
-  // TODO make sure all nodes handles requests and responses
-  // i.e. even candidates nodes should send back log response
-  // in case of the node is with an outdated term
+  onVoteRequest(request: VoteRequest): void {
+    // do nothing
+  }
 
   protected onVoteResponse(response: VoteResponse): void {
-    this.adjustTermIfNewer(response.term);
+    // do nothing
   }
 
   protected onLogRequest(request: LogRequest): void {
-    this.adjustTermIfNewer(request.term);
+    // do nothing
   }
 
   protected onLogResponse(response: LogResponse): void {
-    // Do nothing by default, only leader is interrested by this network event
+    // do nothing
   }
 
   protected quorumReached(numberOfAcks: number): boolean {
