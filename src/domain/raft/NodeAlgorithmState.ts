6cfb234c9bcb09e51d55a1542b67a443a66ed668
Alexandre Fruchaud <alexandre.fruchaud@gmail.com>
2021-10-21 23:24:54 +0200

    Added hello world

--- src/domain/raft/NodeAlgorithmState.ts
+++ src/domain/raft/NodeAlgorithmState.ts
@@ -1,3 +1,7 @@
 import { AbstractNodeAlgorithmState } from "@/domain/raft/states/AbstractNodeAlgorithmState";
 
-export abstract class NodeAlgorithmState extends AbstractNodeAlgorithmState {}
+export abstract class NodeAlgorithmState extends AbstractNodeAlgorithmState {
+  helloWorld(): void {
+    // Do nothing
+  }
+}
