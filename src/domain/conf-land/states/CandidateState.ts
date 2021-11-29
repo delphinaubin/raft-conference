b39c485ce52e6fe0c5f50f7f54356c7d9491d062
Delphin Aubin <delphin_5@msn.com>
2021-11-29 18:22:43 +0100

    feat: end of conference

--- src/domain/conf-land/states/CandidateState.ts
+++ src/domain/conf-land/states/CandidateState.ts
@@ -1,5 +1,60 @@
 import { NodeAlgorithmState } from "@/domain/conf-land/states/NodeAlgorithmState";
+import { VoteRequestBuilder } from "@/domain/framework/network/VoteRequestBuilder";
+import {
+  LogRequest,
+  VoteRequest,
+  VoteResponse,
+} from "@/domain/framework/network/NetworkRequest";
 
 export class CandidateState extends NodeAlgorithmState {
   name = "candidate" as const;
+
+  onEnterInState(): void {
+    super.onEnterInState();
+    this.startElection();
+  }
+
+  private startElection() {
+    this.nodeMemoryState.term++;
+    this.nodeMemoryState.votedFor = this.nodeId;
+    this.nodeMemoryState.nodesWhichVotedForMe.clear();
+    this.nodeMemoryState.nodesWhichVotedForMe.add(this.nodeId);
+    this.sendNetworkRequestToAllOtherNodes(
+      VoteRequestBuilder.aVoteRequest().withTerm(this.nodeMemoryState.term)
+    );
+    this.startTimerWithRandomDuration("election timeout", 500).then(() => {
+      this.startElection();
+    });
+  }
+
+  protected onLogRequest(request: LogRequest): void {
+    super.onLogRequest(request);
+    if (request.term >= this.nodeMemoryState.term) {
+      this.nodeMemoryState.term = request.term;
+      this.changeState("follower");
+    }
+  }
+
+  protected onVoteRequest(request: VoteRequest): void {
+    super.onVoteRequest(request);
+    if (request.term > this.nodeMemoryState.term) {
+      this.nodeMemoryState.term = request.term;
+      this.nodeMemoryState.votedFor = undefined;
+      this.nodeMemoryState.nodesWhichVotedForMe.clear();
+      this.changeState("follower");
+    }
+  }
+
+  protected onVoteResponse(response: VoteResponse): void {
+    super.onVoteResponse(response);
+    if (response.granted) {
+      this.nodeMemoryState.nodesWhichVotedForMe.add(response.senderNodeId);
+      const iHaveTheMajority =
+        this.nodeMemoryState.nodesWhichVotedForMe.size >
+        this.allNodesIds.length / 2;
+      if (iHaveTheMajority) {
+        this.changeState("leader");
+      }
+    }
+  }
 }
--- src/domain/conf-land/states/LeaderState.ts
+++ src/domain/conf-land/states/LeaderState.ts
@@ -1,5 +1,76 @@
 import { NodeAlgorithmState } from "@/domain/conf-land/states/NodeAlgorithmState";
+import {
+  BroadcastRequest,
+  LogRequest,
+  LogResponse,
+  VoteRequest,
+} from "@/domain/framework/network/NetworkRequest";
+import { LogRequestBuilder } from "@/domain/framework/network/LogRequestBuilder";
+import { VoteRequestBuilder } from "@/domain/framework/network/VoteRequestBuilder";
+import { VoteResponseBuilder } from "@/domain/framework/network/VoteResponseBuilder";
 
 export class LeaderState extends NodeAlgorithmState {
   name = "leader" as const;
+
+  onEnterInState() {
+    super.onEnterInState();
+    this.replicateMyLogs();
+  }
+
+  protected onVoteRequest(request: VoteRequest): void {
+    super.onVoteRequest(request);
+    if (request.term > this.nodeMemoryState.term) {
+      this.nodeMemoryState.term = request.term;
+      this.nodeMemoryState.votedFor = request.senderNodeId;
+      this.sendNetworkRequest(
+        VoteResponseBuilder.aVoteResponse()
+          .withReceiverNodeId(request.senderNodeId)
+          .withGranted(true)
+      );
+      this.changeState("follower");
+    }
+  }
+
+  protected onBroadcastRequest(request: BroadcastRequest): void {
+    super.onBroadcastRequest(request);
+    this.appendNewLog(request.log);
+    this.nodeMemoryState.ackedLength[this.nodeId] =
+      this.nodeMemoryState.log.length;
+  }
+
+  protected onLogRequest(request: LogRequest): void {
+    super.onLogRequest(request);
+    if (request.term > this.nodeMemoryState.term) {
+      this.nodeMemoryState.term = request.term;
+      this.changeState("follower");
+    }
+  }
+
+  protected onLogResponse(response: LogResponse): void {
+    super.onLogResponse(response);
+
+    if (response.success) {
+      this.nodeMemoryState.ackedLength[response.senderNodeId] =
+        response.ackLength;
+
+      const nodesWhichAreUpToDate = Object.values(
+        this.nodeMemoryState.ackedLength
+      ).filter((ack) => ack === this.nodeMemoryState.log.length);
+
+      const numberOfNodesUpToDate = nodesWhichAreUpToDate.length;
+      if (numberOfNodesUpToDate > this.allNodesIds.length / 2) {
+        this.nodeMemoryState.commitLength = this.nodeMemoryState.log.length;
+      }
+    }
+  }
+
+  private replicateMyLogs() {
+    this.sendNetworkRequestToAllOtherNodes(
+      LogRequestBuilder.aLogRequest()
+        .withLogEntries(this.nodeMemoryState.log)
+        .withTerm(this.nodeMemoryState.term)
+        .withLeaderCommit(this.nodeMemoryState.commitLength)
+    );
+    this.startTimer(3_000, "replicate logs").then(() => this.replicateMyLogs());
+  }
 }
