a2b405b24b9f780bef76d1d4dd22de1f651a0ee0
Delphin Aubin <delphin_5@msn.com>
2021-11-29 18:22:43 +0100

    feat: end of conference

--- src/domain/conf-land/nodesToCreate.ts
+++ src/domain/conf-land/nodesToCreate.ts
@@ -1,5 +1,7 @@
 import { RaftNode } from "@/domain/framework/RaftNode";
 
 export const nodesToCreate: RaftNode[] = [
-  { id: "1", name: "Node 1", state: "off" },
+  { id: "1", name: "Stark", state: "off" },
+  { id: "2", name: "Lannister", state: "off" },
+  { id: "3", name: "Targaryen", state: "off" },
 ];
